2d25b9488f28ed8553fe5637c61443490ca1c99e
priyak-sdei <priya.kumari@smartdatainc.net>
2025-07-03 21:22:22 +0530

    chore(Rename):App rename script DONE

--- android/app/src/main/java/com/baseSetup/MainActivity.kt
+++ android/app/src/main/java/com/baseSetup/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.smartplatformrn
+package com.baseSetup
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/baseSetup/MainApplication.kt
+++ android/app/src/main/java/com/baseSetup/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.smartplatformrn
+package com.baseSetup
 
 import android.app.Application
 import com.facebook.react.PackageList
