5ee36c254bdb26d1fa7a86c905070107288cdab0
Anil <anilkumar@smartdatainc.net>
2025-07-17 10:04:46 +0530

    updated date

--- pods/social/meta.ts
+++ pods/social/meta.ts
@@ -1,6 +1,6 @@
 export const socialPodMeta = {
   version: '1.0.0',
-  lastUpdated: '2024-07-06', // YYYY-MM-DD
+  lastUpdated: '2024-07-17', // YYYY-MM-DD
   author: 'Anil Kumar',
   maintainedBy: 'SmartData',
 };
